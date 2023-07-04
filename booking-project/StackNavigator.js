38641f1696f7369c5edfaf287990d5671ae9cf52
Gabriel Andrade <99212400+Suntzu1000@users.noreply.github.com>
2023-07-04 16:51:24 -0300

    Estruturando Tela Inicial

--- booking-project/StackNavigator.js
+++ booking-project/StackNavigator.js
@@ -1,25 +1,98 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/HomeScreen";
+import SavedScreen from "./screens/SavedScreen";
+import { AntDesign } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import BookingScreen from "./screens/BookingScreen";
+import { Ionicons } from "@expo/vector-icons";
+import ProfileScreen from "./screens/ProfileScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 const StackNavigator = () => {
-    const Tab = createBottomTabNavigator();
-    const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+  const Stack = createNativeStackNavigator();
 
-    function BottomTabs(){
-        return(
-            <Tab.Navigator>
-                <Tab.Screen name='Inicial' />
-            </Tab.Navigator>
-        )
-    }
+  function BottomTabs() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Inicial"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Inicial",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Entypo name="home" size={24} color="#003580" />
+              ) : (
+                <AntDesign name="home" size={24} color="black" />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Salvo"
+          component={SavedScreen}
+          options={{
+            tabBarLabel: "Salvo",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <AntDesign name="heart" size={24} color="#003580" />
+              ) : (
+                <AntDesign name="hearto" size={24} color="black" />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Reservas"
+          component={BookingScreen}
+          options={{
+            tabBarLabel: "Reservas",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="notifications" size={24} color="#003580" />
+              ) : (
+                <Ionicons
+                  name="notifications-outline"
+                  size={24}
+                  color="black"
+                />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Perfil"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "Perfil",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="person" size={24} color="#003580" />
+              ) : (
+                <Ionicons name="person-outline" size={24} color="black" />
+              ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
   return (
-    <View>
-      <Text>StackNavigator</Text>
-    </View>
-  )
-}
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Main" component={BottomTabs} options={{headerShown: false}} />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+};
 
-export default StackNavigator
+export default StackNavigator;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- booking-project/screens/HomeScreen.js
+++ booking-project/screens/HomeScreen.js
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation;
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      title: "Reserva.com",
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "white",
+      },
+    });
+  }, []);
   return (
     <View>
       <Text>HomeScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
